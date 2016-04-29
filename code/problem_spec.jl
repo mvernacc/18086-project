1dd41e9a3e7b666bb14e4dddd2c945beff441b4e
Matthew Vernacchia <mvernacc@mit.edu>
2016-04-28 23:16:58 -0400

    Start problem spec type

---
+++ code/problem_spec.jl
@@ -0,0 +1,16 @@
+type ProblemSpec
+    # The gas properties
+    gas::Gas
+
+    # Step sizes
+    Δt::Float64
+    Δx::Float64
+    Δy::Float64
+
+    # Boundary condition definition:
+    # Functions which map edge cell U's to ghost U's
+    top_bound::Function
+    right_bound::Function
+    bottom_bound::Function
+    left_bound::Function
+end
