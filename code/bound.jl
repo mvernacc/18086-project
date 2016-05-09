11f44d102708d529fb94ff8bed1b7823066073bd
Matthew Vernacchia <mvernacc@mit.edu>
2016-05-09 16:12:20 -0400

    Add multi-level bound paddding.

--- code/bound.jl
+++ code/bound.jl
@@ -95,18 +95,24 @@ Pad a grid with boundary ghost cells.
 function pad_bounds(U::Array{Float64,3}, top_bound::Function,
     right_bound::Function, bottom_bound::Function, left_bound::Function;
     level=1)
+
+    if level > 1
+        U = pad_bounds(U, top_bound, right_bound, bottom_bound, left_bound,
+                level=level - 1)
+    end
+
     U_pad = zeros(size(U, 1) + 2, size(U, 2) + 2, 4)
     for i in level:size(U, 1) - level + 1
-        U_pad[i+1, end, :] = top_bound(squeeze(U[i, end - level + 1, :], (1,2)))
+        U_pad[i+1, end, :] = top_bound(squeeze(U[i, end - 2 * level + 2, :], (1,2)))
     end
     for j in level:size(U, 2) - level + 1
-        U_pad[end, j+1, :] = right_bound(squeeze(U[end - level + 1, j, :], (1,2)))
+        U_pad[end, j+1, :] = right_bound(squeeze(U[end - 2 * level + 2, j, :], (1,2)))
     end
     for i in level:size(U, 1)  - level + 1
-        U_pad[i+1, 1, :] = bottom_bound(squeeze(U[i, level, :], (1,2)))
+        U_pad[i+1, 1, :] = bottom_bound(squeeze(U[i, 2 * level - 1, :], (1,2)))
     end
     for j in level:size(U, 2) - level + 1
-        U_pad[1, j+1, :] = left_bound(squeeze(U[level, j, :], (1,2)))
+        U_pad[1, j+1, :] = left_bound(squeeze(U[2 * level - 1, j, :], (1,2)))
     end
     U_pad[2:end-1, 2:end-1, :] = U
     return U_pad
--- code/bound_test.jl
+++ code/bound_test.jl
@@ -43,8 +43,59 @@ function test_pad_bounds()
     @test U_pad[1, 2, 1] == 1.4
     @test U_pad[1, 3, 1] == 2.4
     @test U_pad[1, 4, 1] == 3.4
-
 end
+
+function test_pad_bounds_level2()
+    A = [1. 2. 3.;
+         4. 5. 6.;
+         7. 8. 9.]
+    U = cat(3, A, A, A, A)
+
+    U_pad = pad_bounds(U,
+        x -> x + 0.1,
+        x -> x + 0.2,
+        x -> x + 0.3,
+        x -> x + 0.4,
+        level=2)
+
+    dump(U_pad)
+
+    # Dimensions
+    @test size(U_pad) == (7,7,4)
+
+    # Top bound
+    @test U_pad[3, end-1, 1] == 3.1
+    @test U_pad[4, end-1, 1] == 6.1
+    @test U_pad[5, end-1, 1] == 9.1
+    @test U_pad[3, end, 1] == 2.1
+    @test U_pad[4, end, 1] == 5.1
+    @test U_pad[5, end, 1] == 8.1
+
+    # Right bound
+    @test U_pad[end-1, 3, 1] == 7.2
+    @test U_pad[end-1, 4, 1] == 8.2
+    @test U_pad[end-1, 5, 1] == 9.2
+    @test U_pad[end, 3, 1] == 4.2
+    @test U_pad[end, 4, 1] == 5.2
+    @test U_pad[end, 5, 1] == 6.2
+
+    # Bottom bound
+    @test U_pad[3, 2, 1] == 1.3
+    @test U_pad[4, 2, 1] == 4.3
+    @test U_pad[5, 2, 1] == 7.3
+    @test U_pad[3, 1, 1] == 2.3
+    @test U_pad[4, 1, 1] == 5.3
+    @test U_pad[5, 1, 1] == 8.3
+
+    # Left bound
+    @test U_pad[2, 3, 1] == 1.4
+    @test U_pad[2, 4, 1] == 2.4
+    @test U_pad[2, 5, 1] == 3.4
+    @test U_pad[1, 3, 1] == 4.4
+    @test U_pad[1, 4, 1] == 5.4
+    @test U_pad[1, 5, 1] == 6.4
+end
+
 
 function test_ghost_wall()
     @test ghost_wall([0, -1., -1., 0], [1., 0.]) ≈ [0, 1., -1., 0]
@@ -52,6 +103,8 @@ function test_ghost_wall()
     @test ghost_wall([0, 100., 0., 0], [1/2^0.5, 1/2^0.5]) ≈ [0, 0., -100., 0]
 end
 
+
 test_pad_bounds()
+test_pad_bounds_level2()
 test_ghost_wall()
 println("Passed all tests.")
