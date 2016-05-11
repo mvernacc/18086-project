7df14ec6657f25ac167fc773dc5c676fbb9c11a8
Matthew Vernacchia <mvernacc@mit.edu>
2016-05-11 00:23:11 -0400

    Add pressure, mach temperature plot.

--- code/vis.jl
+++ code/vis.jl
@@ -7,7 +7,7 @@
 using PyPlot
 using CFD086
 
-export plot_U
+export plot_U, plot_pTM
 
 function plot_U(U, ps::ProblemSpec)
     titles = ["Density", "x Momentum", "y Momentum", "Energy"]
@@ -25,6 +25,39 @@ function plot_U(U, ps::ProblemSpec)
     for i in 1:4
         subplot(2,2,i)
         pcolormesh(X', Y', U[:,:,i]')
+        title(titles[i])
+        xlabel("x [meter]")
+        ylabel("y [meter]")
+        colorbar()
+    end
+end
+
+function plot_pTM(U, ps::ProblemSpec)
+    titles = ["Pressure [kPa]", "Mach number [-]", "Temperature [K]"]
+
+    X = zeros(size(U,1), size(U,2))
+    Y = zeros(size(U,1), size(U,2))
+
+    for i in 1:size(U,1)
+        for j in 1:size(U,2)
+            X[i, j] = ps.x(i * ps.Δx, j * ps.Δy)
+            Y[i, j] = ps.y(i * ps.Δx, j * ps.Δy)
+        end
+    end
+
+    result = zeros(size(U,1), size(U,2), 3)
+
+     for i in 1:size(U,1)
+        for j in 1:size(U,2)
+            result[i, j, 1] = u2p(U[i,j,:], ps.gas) * 1e-3
+            result[i, j, 2] = u2M(U[i,j,:], ps.gas)
+            result[i, j, 3] = u2T(U[i,j,:], ps.gas)
+        end
+    end
+
+    for i in 1:3
+        subplot(2,2,i)
+        pcolormesh(X', Y', result[:,:,i]')
         title(titles[i])
         xlabel("x [meter]")
         ylabel("y [meter]")
